4e69c8ae00b66a75d11765ff831a60d2361713da
willyhorizont <willyhorizont@gmail.com>
2026-05-28 00:08:44 +0700

    finish vscode_ctrl_shift_up_or_down

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# willyhorizont-sublimetext-plugin
-willyhorizont-sublimetext-plugin
+# willyhorizont-sublimetext-plugin  
+
+## Sublimetext with VS Code feel | Sublimetext vscode-ify | vscode keyboard shortcut/keybinding in Sublimetext  
+
+[  
+    "ctrl+`",  
+    "ctrl+alt+up",  
+    "ctrl+alt+down",  
+    "ctrl+shift+up",  
+    "ctrl+shift+down",  
+    "alt+up",  
+    "alt+down",  
+    "shift+alt+up",  
+    "shift+alt+down",  
+].map((k) => ("vscode keyboard (shortcut|keybinding)" + k + " in Sublimetext")).join("\n")  
+
+## Installation  
+Replace All ~/.config/sublime-text/Packages/User  
+
+
+## Author  
+[willyhorizont.github.io](willyhorizont.github.io)  
+
+## keyword:  
+Sublimetext  
+Sublime Text  
+VSCode  
+VS Code  
+keyboard shortcut  
+keybinding  
+keymap  
--- willyhorizont-sublimetext-plugin.py
+++ willyhorizont-sublimetext-plugin.py
@@ -2,6 +2,12 @@ import sublime
 import sublime_plugin
 
 
+def create_logger(show_log=False):
+    if not show_log:
+        return lambda _: None
+    return print
+
+
 def clear_console():
     settings = sublime.load_settings("Preferences.sublime-settings")
     scrollback = settings.get("console_max_history_lines")
@@ -36,110 +42,88 @@ def vscode_duplicate_selected_down(self, edit):
             self.view.insert(edit, region.end(), text)
 
 
-def sublime_text_select_lines(self, edit, forward=True):
-    # print("\n" * 5)
-    # print("\n" * 100)
-    # self.view.run_command("willyhorizont_utils", {"action": "clear_console"})
-    # sublime.run_command("willyhorizont_app_utils", {"action": "clear_console"})
+def vscode_ctrl_shift_up_or_down(self, edit, forward=True, show_log=False):
+    log = create_logger(show_log)
     self.view.window().run_command("willyhorizont_utils", {"action": "clear_console"})
 
     settings = self.view.settings()
-    # settings.set("target_prev_col", 0)
     target_prev_col = settings.get("target_prev_col")
-    print(f"target_prev_col={target_prev_col}")
+    log(f"target_prev_col={target_prev_col}")
 
     current_selections = list(self.view.sel())
 
     if not current_selections:
         return
 
-    print(f"current_selections={current_selections}")
-    print(f"current_selections[-1]={current_selections[-1]}")
-    print(f"current_selections[0]={current_selections[0]}")
+    log(f"current_selections={current_selections}")
+    log(f"current_selections[-1]={current_selections[-1]}")
+    log(f"current_selections[0]={current_selections[0]}")
 
     current_selections_text = "".join([self.view.substr(region) for region in current_selections])
-    print(f"current_selections_text: \"\"\"{current_selections_text}\"\"\"")
+    log(f"current_selections_text=\"\"\"{current_selections_text}\"\"\"")
     current_selections_text_length = len(current_selections_text)
-    print(f"current_selections_text_length={current_selections_text_length}")
+    log(f"current_selections_text_length={current_selections_text_length}")
 
     current_selection = current_selections[-1] if forward else current_selections[0]
-    print(f"current_selection={current_selection}")
-    print(f"current_selection.a={current_selection.a}")
-    print(f"current_selection.b={current_selection.b}")
+    log(f"current_selection={current_selection}")
+    log(f"current_selection.a={current_selection.a}")
+    log(f"current_selection.b={current_selection.b}")
 
     current_row, current_col = self.view.rowcol(current_selection.b)
     current_row += 1
     current_col += 1
-    print(f"current_row={current_row}")
-    print(f"current_col={current_col}")
+    log(f"current_row={current_row}")
+    log(f"current_col={current_col}")
 
-    target_row = current_row + (1 if forward else -1)
-    print(f"target_row={target_row}")
-    # target_prev_col = settings.get("target_prev_col")
-    # print(f"target_prev_col={target_prev_col}")
-    # if not target_prev_col:
-    #     settings.set("target_prev_col", current_col)
+    target_row = (current_row + (1 if forward else -1))
+    log(f"target_row={target_row}")
     target_col = current_col
-    print(f"target_col={target_col}")
+    log(f"target_col={target_col}")
 
     target_text_point = self.view.text_point((target_row - 1), 0)
-    print(f"target_text_point={target_text_point}")
+    log(f"target_text_point={target_text_point}")
     target_line = self.view.line(target_text_point)
-    print(f"target_line={target_line}")
+    log(f"target_line={target_line}")
     target_line_text = self.view.substr(target_line)
-    print(f"target_line_text: \"\"\"{target_line_text}\"\"\"")
+    log(f"target_line_tex=\"\"\"{target_line_text}\"\"\"")
     target_line_length = target_line.size()
-    print(f"target_line_length={target_line_length}")
+    log(f"target_line_length={target_line_length}")
 
     new_target_text_point = 0
-    print(f"new_target_text_point_init={new_target_text_point}, target_col={target_col}")
+    log(f"new_target_text_point_init={new_target_text_point}, target_col={target_col}")
     if (target_col > target_line_length):
-        # if target_prev_col:
-        #     target_text_point = self.view.text_point((target_row - 1), target_prev_col)
         new_target_text_point = (target_text_point + target_line_length)
-        print(f"new_target_text_point_if={new_target_text_point}")
+        log(f"new_target_text_point_if={new_target_text_point}")
         if (target_line_length > 0):
             settings.set("target_prev_col", target_col)
-            print(f"target_prev_col set to {target_col}")
+            log(f"target_prev_col set to {target_col}")
     else:
         new_target_text_point = (target_text_point + (target_col - 1))
         if target_prev_col:
             target_text_point = self.view.text_point((target_row - 1), target_prev_col)
             new_target_text_point = (target_text_point - 1)
-        print(f"new_target_text_point_else={new_target_text_point}, target_col={target_col}")
+        log(f"new_target_text_point_else={new_target_text_point}, target_col={target_col}")
         settings.set("target_prev_col", None)
-        print(f"target_prev_col set to {None}")
-    print(f"new_target_text_point_final={new_target_text_point}")
+        log(f"target_prev_col set to {None}")
+    log(f"new_target_text_point_final={new_target_text_point}")
 
     target_text_point = new_target_text_point
-    print(f"target_text_point={target_text_point}")
+    log(f"target_text_point={target_text_point}")
     target_region = sublime.Region(target_text_point, (target_text_point + (target_line_length - (target_col - 1))))
-    print(f"target_region={target_region}")
+    log(f"target_region={target_region}")
     target_region_text = self.view.substr(target_region)
-    print(f"target_region_text: \"\"\"{target_region_text}\"\"\"")
+    log(f"target_region_text=\"\"\"{target_region_text}\"\"\"")
     target_region_length = target_region.size()
-    print(f"target_region_length={target_region_length}")
-
-    # current_selections.insert(0, target_region)
-    # self.view.sel().add(target_region)
-
-    # self.view.sel().clear()
-
-    # self.view.sel().add(target_region)
-
-    # for region in current_selections:
-    #     self.view.sel().add(region)
+    log(f"target_region_length={target_region_length}")
 
     new_region_text_point = new_target_text_point
-    print(f"new_region_text_point={new_region_text_point}")
+    log(f"new_region_text_point={new_region_text_point}")
     new_region = sublime.Region(current_selection.a, new_target_text_point)
-    print(f"new_region={new_region}")
+    log(f"new_region={new_region}")
     new_region_text = self.view.substr(new_region)
-    print(f"new_region_text: \"\"\"{new_region_text}\"\"\"")
+    log(f"new_region_text=\"\"\"{new_region_text}\"\"\"")
     new_region_length = new_region.size()
-    print(f"new_region_length={new_region_length}")
-
-    # self.view.sel().add(region)
+    log(f"new_region_length={new_region_length}")
 
     if forward:
         current_selections[-1] = new_region
@@ -163,8 +147,8 @@ class WillyhorizontKeyboardShortcutCommand(sublime_plugin.TextCommand):
         if action == "vscode_duplicate_selected_down":
             vscode_duplicate_selected_down(self, edit)
             return
-        if action == "sublime_text_select_lines":
-            sublime_text_select_lines(self, edit, forward=kwargs.get("forward"))
+        if action == "vscode_ctrl_shift_up_or_down":
+            vscode_ctrl_shift_up_or_down(self, edit, forward=kwargs.get("forward"))
             return
         if action == "do_nothing":
             return
